c14a5d57655bf4f0da191bd0a9f1ff191922bd6a
Daniel Stotts <dstodev@gmail.com>
2019-08-27 22:50:26 -0700

    Before diving into dll symbol export stuff

---
+++ project/lib/MSDL/include/msdl_event.h
@@ -0,0 +1,17 @@
+// #ifndef MSDL_EVENT_H
+// #define MSDL_EVENT_H
+
+// #include <SDL.h>
+
+// class MSDL_EventController
+// {
+// public:
+// 	MSDL_EventController() = default;
+// 	~MSDL_EventController() = default;
+
+// 	bool await_event(uint32_t type, uint32_t timeout_ms);
+
+// private:
+// };
+
+// #endif
